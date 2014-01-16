f79a3a599bc8c0d4ee92c8f5f5b2fc20dacadd92
SURESH <suresh@SURESHs-Mac-mini.local>
2014-01-16 11:03:31 +0530

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by SURESH on 16/01/14.
+//  Copyright (c) 2014 VVDN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by SURESH on 16/01/14.
+//  Copyright (c) 2014 VVDN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
